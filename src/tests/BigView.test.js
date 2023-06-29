a19a427b3bea5cbf777d8cc5084709b5a996569a
francksefu <90141102+francksefu@users.noreply.github.com>
2023-06-29 22:14:35 +0200

    correct linters

--- src/tests/BigView.test.js
+++ src/tests/BigView.test.js
@@ -1,6 +1,6 @@
-import BigView from "../components/BigView";
-import BigViewItem from "../components/BigViewItem";
-import DetailViewItem from "../components/DetailViewItem";
+import BigView from '../components/BigView';
+import BigViewItem from '../components/BigViewItem';
+import DetailViewItem from '../components/DetailViewItem';
 
 test('it renders correclty', () => {
   const component = DetailViewItem;
